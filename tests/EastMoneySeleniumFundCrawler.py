1255917f8e76958610114ae96bc36c3a9fcbc256
minicloudsky <1397991131@qq.com>
2020-09-20 01:05:27 +0800

    数据库涉及优化

--- tests/EastMoneySeleniumFundCrawler.py
+++ tests/EastMoneySeleniumFundCrawler.py
@@ -1,15 +1,19 @@
 import datetime
+import logging
+import math
 import random
 import time
 
 from bs4 import BeautifulSoup
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
-from apps.Fund.models import Fund
+
+from apps.Fund.models import FundHistoricalNetWorthRanking, FundLog
+
+logger = logging.getLogger("eastmoney")
+
 
 # 东方财富基金
-
-
 class EastMoneyFund:
     fundrank_url = 'http://fund.eastmoney.com/data/fundranking.html'
     chrome_options = Options()
@@ -20,43 +24,49 @@ class EastMoneyFund:
     chrome_options.add_argument('--disable-gpu')
     driver = Chrome(chrome_options=chrome_options)
     year = datetime.datetime.now().year
-    funds = []
+    total_funds = 0
+    default_max_page = 200
 
     def __init__(self):
         self.get_fund(self.fundrank_url)
 
     def get_fund(self, url):
-        print("Start crawl easymoney fund .")
-        self.driver.get(url)
-        next_btn_xpath = self.element_is_exist()
-        time.sleep(random.randint(4, 10))
-        self.parse_html(self.driver.page_source)
-        while next_btn_xpath:
-            self.driver.find_element_by_xpath(next_btn_xpath).click()
-            time.sleep(random.randint(4, 10))
-            self.parse_html(self.driver.page_source)
-            next_btn_xpath = self.element_is_exist()
-        self.driver.close()
-        print("Crawl eastmoney fund finished .")
-
-    def element_is_exist(self):
+        logger.info("Start crawl easymoney fund .")
+        start_time = datetime.datetime.now()
         next_btn_xpath = '//*[@id="pagebar"]/label[8]'
         backup_next_btn_xpath = '//*[@id="pagebar"]/label[9]'
-        try:
-            element = self.driver.find_element_by_xpath(next_btn_xpath)
-            print("element {} element.text {}".format(element, element.text))
-            if element and element.text == '下一页':
-                return next_btn_xpath
-            else:
-                element = self.driver.find_element_by_xpath(
-                    backup_next_btn_xpath)
-                print("element {} element.text {}".format(element, element.text))
+        self.driver.get(url)
+        time.sleep(random.randint(9, 15))
+        self.parse_html(self.driver.page_source)
+        total_page = self.get_total_page()
+        for i in range(total_page):
+            try:
+                element = self.driver.find_element_by_xpath(next_btn_xpath)
                 if element and element.text == '下一页':
-                    return backup_next_btn_xpath
-            return None
-        except Exception as e:
-            print("exception! {}".format(e))
-            return None
+                    self.driver.find_element_by_xpath(next_btn_xpath).click()
+                else:
+                    element = self.driver.find_element_by_xpath(backup_next_btn_xpath)
+                    if element and element.text == '下一页':
+                        self.driver.find_element_by_xpath(backup_next_btn_xpath).click()
+            except Exception as e:
+                logger.warning("exception! {}".format(e))
+            time.sleep(random.randint(9, 15))
+            self.parse_html(self.driver.page_source)
+        self.driver.close()
+        end_time = datetime.datetime.now()
+        FundLog.objects.create(start_time=start_time, end_time=end_time, total_fund=self.total_funds)
+        logger.info("Crawl eastmoney fund finished .")
+
+    def get_total_page(self):
+        element = self.driver.find_element_by_xpath('//*[@id="types"]/li[1]')
+        if element:
+            logger.info(element.text)
+            total = element.text.replace('全部', '').replace('(', '').replace(')', '').replace(' ', '')
+            page = math.ceil(int(total) / 50) if total else 200
+            logger.info("total page: {}".format(page))
+            return page
+        else:
+            return self.default_max_page
 
     def to_int(self, val):
         try:
@@ -66,6 +76,7 @@ class EastMoneyFund:
                 return val
             return -9999
         except Exception as e:
+            logger.warning("parsing number error! {} - {}".format(val, e))
             val = -9999
         return val
 
@@ -77,6 +88,7 @@ class EastMoneyFund:
                 return val
             return -9999
         except Exception as e:
+            logger.warning("parsing number error! {} - {}".format(val, e))
             val = -9999
         return val
 
@@ -90,7 +102,7 @@ class EastMoneyFund:
             return str(datetime.datetime.now())
         except Exception as e:
             val = str(datetime.datetime.now())
-            print("format date error {}".format(e))
+            logger.warning("format date error {}".format(e))
             return val
 
     def parse_html(self, html):
@@ -99,6 +111,7 @@ class EastMoneyFund:
         table = table[0] if table else ''
         table = table.tbody if table else ''
         trs = list(table.children) if table else []
+        funds = []
         # tr里面一行是一个基金的详细数据
         for tr in trs:
             fund = {}
@@ -121,14 +134,17 @@ class EastMoneyFund:
             fund['this_year'] = self.to_float(tds[15].string)
             fund['since_founded'] = self.to_float(tds[16].string)
             fund['handling_fee'] = self.to_float(tds[18].string)
-            is_exist = Fund.objects.filter(update_date=fund['update_date'],
-                                           fund_code=fund['fund_code'],
-                                           fund_url=fund['fund_url'],
-                                           fund_name=fund['fund_name']).first()
+            is_exist = FundHistoricalNetWorthRanking.objects.filter(update_date=fund['update_date'],
+                                                                    fund_code=fund['fund_code'],
+                                                                    fund_url=fund['fund_url'],
+                                                                    fund_name=fund['fund_name']).first()
             if not is_exist:
-                Fund.objects.create(**fund)
+                funds.append(FundHistoricalNetWorthRanking(**fund))
             else:
-                print(self.driver.current_url)
+                logger.warning(
+                    "{}-{}-{} exists!".format(fund['fund_code'], fund['fund_name'], str(fund['update_date'])))
+        FundHistoricalNetWorthRanking.objects.bulk_create(funds)
+        self.total_funds += len(funds)
 
 
 if __name__ == '__main__':
