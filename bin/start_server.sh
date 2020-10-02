dfe63cc95302a53f5e9a10bc907adb838f14dc20
minicloudsky <1397991131@qq.com>
2020-10-02 16:10:35 +0800

    添加开放式基金和货币基金爬虫

--- bin/start_server.sh
+++ bin/start_server.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-PATH = pwd
-cd $PATH
 
-sh eastmoney_uwsgi.sh start
-sh nohup_run_nodejs_server.sh
-sh nohup_run_spider.sh
+sh ./bin/eastmoney_uwsgi.sh start
+sh ./bin/nohup_run_nodejs_server.sh
+sh ./bin/nohup_run_spider.sh
