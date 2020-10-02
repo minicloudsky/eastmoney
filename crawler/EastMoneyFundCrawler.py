c29274124aa659d5e8ce2e29fbee2085fae0bdf0
minicloudsky <1397991131@qq.com>
2020-10-02 12:49:50 +0800

    完成基金经理和香港基金爬虫

--- crawler/EastMoneyFundCrawler.py
+++ crawler/EastMoneyFundCrawler.py
@@ -1,12 +1,14 @@
-import logging
-from datetime import datetime
-from urllib.parse import urlencode
-import threading
-import requests
-import os
-from apps.Fund.models import FundHistoricalNetWorthRanking, FundLog, Fund, FundCompany
-from multiprocessing.dummy import Pool as ThreadPool
 import copy
+import logging
+import os
+import threading
+from datetime import datetime
+from multiprocessing.dummy import Pool as ThreadPool
+from urllib.parse import urlencode
+
+import requests
+
+from apps.Fund.models import FundHistoricalNetWorthRanking, FundLog, Fund, FundCompany, FundManager
 
 logger = logging.getLogger("easymoneyfundcrawler")
 
@@ -35,6 +37,10 @@ class EastMoneyFund:
     history_net_worth_url = 'http://api.fund.eastmoney.com/f10/lsjz?'
     fund_company_url = 'http://fund.eastmoney.com/Company/default.html'
     monetary_fund_url = 'http://api.fund.eastmoney.com/FundRank/GetHbRankList?intCompany=0&MinsgType=&IsSale=0&strSortCol=SYL_Y&orderType=desc&pageIndex=1&pageSize=500000&_=1601519558625'
+    asset_manage_fund_url = 'http://api.fund.eastmoney.com/FundRank/GetLcRankList?intCompany=0&MinsgType=undefined' \
+                            '&IsSale=0&strSortCol=SYL_Z&orderType=desc&pageIndex={}&pageSize={}&FBQ='.format(
+        1, 500000)
+    overseas_fund_url = 'http://overseas.1234567.com.cn/overseasapi/OpenApiHander.ashx?api=HKFDApi&m=MethodFundList&action=1&pageindex={}&pagesize={}&dy=1&date1=1990-10-02&date2={}&sortfield=W&sorttype=-1&isbuy=0'
 
     def __init__(self):
         FundLog.objects.create(name="开始爬取东方财富基金数据", start_time=datetime.now(), end_time=datetime.now())
@@ -44,6 +50,9 @@ class EastMoneyFund:
         self.schedule_history_net_worth()
         self.get_fbs_fund_ranking()
         self.get_monetary_fund_ranking()
+        self.get_asset_manage_fund_ranking()
+        self.get_hongkong_fund_ranking()
+        self.get_fund_manager()
         logger.warning("------{} All crawling task finished".format(datetime.now()))
         FundLog.objects.create(
             name="爬取东方财富基金数据完成", start_time=datetime.now(), end_time=datetime.now())
@@ -54,7 +63,7 @@ class EastMoneyFund:
         log_kwargs = {}
         log_kwargs['name'] = '爬取基金排行'
         log_kwargs['start_time'] = datetime.now()
-        response = requests.get(url)
+        response = self.get(url)
         funds_json = copy.copy(response.json())
         if funds_json:
             all_fund = copy.copy(funds_json['datas'])
@@ -114,7 +123,7 @@ class EastMoneyFund:
         url = self.nodejs_server_url + "diy_fund_ranking"
         log_kwargs = {}
         log_kwargs['name'] = '爬取基金从成立以来的净值和分红情况'
-        response = requests.get(url)
+        response = self.get(url)
         funds_json = copy.copy(response.json())
         if funds_json:
             all_fund = funds_json['datas']
@@ -165,7 +174,8 @@ class EastMoneyFund:
 
     def schedule_history_net_worth(self):
         logger.info("{} start multi thread crawl history net worth.".format(datetime.now()))
-        fund_codes = Fund.objects.all().values('fund_code')
+        fund_codes = Fund.objects.all()
+        fund_codes = [fund_code for fund_code in fund_codes if fund_code.fund_type != 'HK']
         fund_codes = [x['fund_code'] for x in fund_codes]
         pool = ThreadPool(self.thread_num)
         # 在每个线程中执行任务
@@ -185,7 +195,7 @@ class EastMoneyFund:
             '_': '1600569328705',
         }
         request_url = self.history_net_worth_url + urlencode(params)
-        response = requests.get(request_url, headers=self.headers)
+        response = self.get(request_url)
         history_net_worth_json = copy.copy(response.json())
         if history_net_worth_json and history_net_worth_json.get('Data').get('LSJZList'):
             history_net_worths = history_net_worth_json.get('Data').get('LSJZList')
@@ -215,7 +225,7 @@ class EastMoneyFund:
                                start_time=datetime.now(), end_time=datetime.now())
         logger.info("{} start crawl fund company .".format(datetime.now()))
         url = self.nodejs_server_url + "fund_company"
-        response = requests.get(url)
+        response = self.get(url)
         funds_company_json = response.json()
         funds_company_object_list = []
         if funds_company_json and funds_company_json.get('datas'):
@@ -256,7 +266,7 @@ class EastMoneyFund:
     def get_fbs_fund_ranking(self):
         logger.warning("start get fbs fund ranking.")
         url = self.nodejs_server_url + "fbs_fund_ranking"
-        response = requests.get(url)
+        response = self.get(url)
         fbs_funds = response.json()
         if not fbs_funds['datas']:
             logger.warning("get_fbs_fund_ranking empty data.")
@@ -299,7 +309,7 @@ class EastMoneyFund:
 
     def get_monetary_fund_ranking(self):
         logger.warning("开始爬取货币基金排行")
-        response = requests.get(self.monetary_fund_url, headers=self.headers)
+        response = self.get(self.monetary_fund_url)
         datas = response.json()
         if not datas:
             logger.warning("货币基金数据为空!")
@@ -342,6 +352,131 @@ class EastMoneyFund:
                 pass
         logger.warning("爬取货币基金完成")
 
+    def get_asset_manage_fund_ranking(self):
+        logger.warning("开始爬取理财基金排行")
+        response = self.get(self.asset_manage_fund_url)
+        datas = response.json()
+        if not datas:
+            logger.warning("理财基金数据为空!")
+            return
+        logger.warning("理财基金总数: {}".format(datas['TotalCount']))
+        for fund in datas['Data']:
+            try:
+                fund_code = fund['FCODE'] if fund['FCODE'] else ''
+                current_date = self.check_date(fund['FSRQ']) if fund['FSRQ'] else self.default_error_date
+                fund_defaults = {
+                    'fund_name': fund['SHORTNAME'],
+                    'pinyin_abbreviation_code': fund['ABBNAME'],
+                    'can_buy': 1 if fund['ISBUY'] == 1 else 0,
+                    'fund_type': fund['FTYPE'],
+                    'update_time': datetime.now(),
+                    'establish_date': self.default_error_date,
+                }
+                defaults = {
+                    'ten_thousand_income': self.to_float(fund['DWJZ']),
+                    'annualized_income_7day': self.to_float(fund['LJJZ']),
+                    'annualized_income_14day': self.to_float(fund['FTYI']),
+                    'annualized_income_28day': self.to_float(fund['TEYI']),
+                    'current_unit_net_worth': self.to_float(fund['JZ']),
+                    'last_month': self.to_float(fund['SYL_Y']),
+                    'last_three_month': self.to_float(fund['SYL_3Y']),
+                    'last_six_month': self.to_float(fund['SYL_6Y']),
+                    'this_year': self.to_float(fund['SYL_JN']),
+                    'since_founded': self.to_float(fund['SYL_LN']),
+                    'handling_fee': self.to_float(fund['RATE']),
+                }
+                Fund.objects.update_or_create(defaults=fund_defaults, **{'fund_code': fund_code})
+                FundHistoricalNetWorthRanking.objects.update_or_create(
+                    defaults=defaults, **{'fund_code': fund_code, 'current_date': current_date})
+            except Exception as e:
+                logger.warning("exception in asset manage fund : {}".format(e))
+                pass
+        logger.warning("爬取理财基金完成")
+
+    def get_hongkong_fund_ranking(self):
+        logger.warning("开始爬取香港基金排行")
+        try:
+            response = self.get(self.overseas_fund_url.format(1, 50, self.date))
+            datas = response.json()
+            if not datas:
+                logger.warning("香港基金数据为空!")
+                return
+            logger.warning("香港基金总数: {}".format(datas['TotalCount']))
+        except Exception as e:
+            logger.warning("Exception in get hk fund {}".format(e))
+        total_page = datas['TotalCount'] / 50 + 1 if datas['TotalCount'] else 10
+        for page in range(int(total_page) + 1):
+            response = self.get(self.overseas_fund_url.format(page, 50, self.date))
+            try:
+                datas = response.json()
+            except Exception as e:
+                logger.warning("Exception in get hk fund {}".format(e))
+                datas = None
+            if not datas:
+                break
+            for fund in datas['Data']:
+                if not datas:
+                    logger.warning("香港基金数据为空!")
+                    return
+                try:
+                    fund_code = fund['FCODE'] if fund['FCODE'] else ''
+                    current_date = self.check_date(fund['JZRQ']) if fund['JZRQ'] else self.default_error_date
+                    fund_defaults = {
+                        'fund_name': fund['FULLNAME'],
+                        'fund_short_name': fund['SHORTNAME'],
+                        'can_buy': 1 if fund['ISBUY'] == 1 else 0,
+                        'fund_type': 'HK',
+                        'currency': fund['CURRENCY'] if fund['CURRENCY'] else '',
+                        'update_time': datetime.now(),
+                        'establish_date': self.default_error_date,
+                    }
+                    defaults = {
+                        'last_week': self.to_float(fund['W']),
+                        'current_unit_net_worth': self.to_float(fund['NAV']),
+                        'last_month': self.to_float(fund['M']),
+                        'last_three_month': self.to_float(fund['Q']),
+                        'last_six_month': self.to_float(fund['HY']),
+                        'last_year': self.to_float(fund['Y']),
+                        'last_two_year': self.to_float(fund['TWY']),
+                        'last_three_year': self.to_float(fund['TRY']),
+                        'this_year': self.to_float(fund['SY']),
+                        'since_founded': self.to_float(fund['SE']),
+                    }
+                    Fund.objects.update_or_create(defaults=fund_defaults, **{'fund_code': fund_code})
+                    FundHistoricalNetWorthRanking.objects.update_or_create(
+                        defaults=defaults, **{'fund_code': fund_code, 'current_date': current_date})
+                except Exception as e:
+                    logger.warning("exception in hk fund : {}".format(e))
+                    pass
+        logger.warning("爬取香港基金完成")
+
+    def get_fund_manager(self):
+        request_url = self.nodejs_server_url + "fund_manager"
+        response = self.get(request_url)
+        datas = response.json()
+        total_manager = datas['record']
+        logger.warning("基金经理总数: {}".format(total_manager))
+        managers = datas['data']
+        for manager in managers:
+            try:
+                defaults = {}
+                manager_id = manager[0] if manager[0] else 0
+                name = manager[1] if manager[1] else '',
+                company_id = manager[2] if manager[2] else 0,
+                managed_funds = manager[4].split(',') if manager[4] else ''
+                working_time = self.to_int(manager[6])
+                current_fund_best_profit = self.to_float(manager[7]) if manager[7] else 0
+                total_asset_manage_amount = self.to_float(manager[10]) if manager[10] else 0
+                defaults['name'] = name[0] if name[0] else ''
+                defaults['company_id'] = company_id[0] if company_id[0] else ''
+                defaults['working_time'] = working_time
+                defaults['current_fund_best_profit'] = current_fund_best_profit
+                defaults['total_asset_manage_amount'] = total_asset_manage_amount
+                FundManager.objects.update_or_create(defaults=defaults,
+                                                     **{'manager_id': manager_id})
+            except Exception as e:
+                logger.warning("parse fund manager error! {}".format(e))
+
     def to_int(self, val):
         try:
             if val:
@@ -356,7 +491,7 @@ class EastMoneyFund:
     def to_float(self, val):
         try:
             if val:
-                val = val.replace('%', '').replace('---', '')
+                val = val.replace('%', '').replace('---', '').replace('亿元', '')
                 val = float(val)
                 return val
             return -9999
@@ -397,6 +532,14 @@ class EastMoneyFund:
             target_list.append(list[cut * i:cut * (1 + i)])
         target_list.append(list[cut * (n - 1):len(list)])
         return target_list
+
+    def get(self, url):
+        try:
+            response = requests.get(url, headers=self.headers)
+            return response
+        except Exception as e:
+            logger.warning("network error! {}".format(e))
+            return {}
 
 
 if __name__ == '__main__':
--- crawler_nodejs/index.js
+++ crawler_nodejs/index.js
@@ -61,6 +61,7 @@ function getFundCompany() {
             console.log(error);
         });
 }
+
 function getFbsFundRanking() {
     let url = "http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=fb&ft=ct&rs=&gs=0&sc=zzf&st=desc&pi=1&pn=500000&v=0.027676829448198603"
     console.log("获取场内基金: " + url)
@@ -69,6 +70,24 @@ function getFbsFundRanking() {
         .then(function (response) {
             let html_string = response.data.toString(); // 获取网页内容
             html_string += "\n rankData;";
+            return Promise.resolve(eval(html_string));
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function getFundManager() {
+    let page = 1
+    let page_size = 100000
+    let url = "http://fund.eastmoney.com/Data/FundDataPortfolio_Interface.aspx?dt=14&mc=returnjson&ft=all&pn="
+        + page_size + "&pi=" + page + "&sc=abbname&st=asc"
+    console.log("获取基金经理: " + url)
+    return axios
+        .get(url)
+        .then(function (response) {
+            let html_string = response.data.toString(); // 获取网页内容
+            html_string += "\n returnjson";
             return Promise.resolve(eval(html_string));
         })
         .catch(function (error) {
@@ -93,14 +112,18 @@ app.get("/", (req, res) => {
         promise.then((response) => {
             res.json(response); // 数据返回
         });
-    } else if(type==='fbs_fund_ranking'){
+    } else if (type === 'fbs_fund_ranking') {
         let promise = getFbsFundRanking(); // 发起抓取
+        promise.then((response) => {
+            res.json(response); // 数据返回
+        });
+    } else if (type === 'fund_manager') {
+        let promise = getFundManager()
         promise.then((response) => {
             res.json(response); // 数据返回
         });
     }
 });
-
 
 
 app.listen(3000, () => console.log("Listening on port 3000!")); // 监听3000端口
