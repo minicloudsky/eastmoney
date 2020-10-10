f05537337af2863bdb9dfa52fcd161f30891745c
minicloudsky <1397991131@qq.com>
2020-10-10 21:52:55 +0800

    shell脚本修改

--- backend/bin/nohup_run_nodejs_server.sh
+++ backend/bin/nohup_run_nodejs_server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_PATH=/home/.jywcode/eastmoneyspider/crawler_nodejs
+PROJECT_PATH=/home/.jywcode/eastmoneyspider/backend/crawler_nodejs
 # shellcheck disable=SC2164
 cd $PROJECT_PATH
 npm install
--- backend/bin/nohup_run_spider.sh
+++ backend/bin/nohup_run_spider.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-PROJECT_PATH=/home/.jywcode/eastmoneyspider
+PROJECT_PATH=/home/.jywcode/eastmoneyspider/backend
 # shellcheck disable=SC2164
 cd $PROJECT_PATH
 source venv/bin/activate
 pip3 install -r requirements.txt
 
-nohup python3 $PROJECT_PATH/manage.py runserver 0:10000  > nohup.out &
+nohup python3 $PROJECT_PATH/manage.py runserver 0:12000  > nohup.out &
 nohup python3 $PROJECT_PATH/run_spider.py > nohup.out & 
