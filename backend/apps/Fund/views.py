e45c705168a8ddb6dbc2c6f4923dcf7cbe1b41df
minicloudsky <1397991131@qq.com>
2020-10-14 15:31:33 +0800

    fix

--- backend/apps/Fund/views.py
+++ backend/apps/Fund/views.py
@@ -1,15 +1,36 @@
 from rest_framework.serializers import ModelSerializer
 from apps.Fund.models import FundLog
 from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
 
 
-class LogSerializer(ModelSerializer):
+class FundLogSerializer(ModelSerializer):
     class Meta:
         model = FundLog
         fields = "__all__"
 
 
-class LogView(APIView):
+class FundLogView(ListAPIView):
+    # permission_classes = [IsAuthenticated, ]
+    pagination_class = PageNumberPagination
+    serializer_class = FundLogSerializer
+    queryset = FundLog.objects.order_by("-start_time").all()
+
     def get(self, request, *args, **kwargs):
         page = request.query_params.get('page') or 1
         page_size = request.query_params.get('page_size') or 10
+        keyword = request.query_params.get('keyword') or ''
+        if keyword:
+            queryset = FundLog.objects.filter(
+                name__contains=keyword).order_by('id')
+        else:
+            queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
--- backend/eastmoneyspider/urls.py
+++ backend/eastmoneyspider/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('log/', include("apps.Fund.urls")),
 ]
