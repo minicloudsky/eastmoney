2baeef952720f8ca4fb44a70543cafac6cddc148
minicloudsky <1397991131@qq.com>
2020-10-16 13:35:21 +0800

    fix

---
+++ backend/bin/nohup_run_django_server.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+PROJECT_PATH=/home/.jywcode/eastmoneyspider/backend
+# shellcheck disable=SC2164
+cd $PROJECT_PATH
+source venv/bin/activate
+pip3 install -r requirements.txt
+
+nohup python3 $PROJECT_PATH/manage.py runserver 0:12000  > nohup.out &
