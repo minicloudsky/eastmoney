5f8233509929cd0990521999950ba3d3e931eb41
minicloudsky <1397991131@qq.com>
2021-01-30 20:27:40 +0800

    bug fix

--- backend/bin/start_server.sh
+++ backend/bin/start_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-sh ./bin/eastmoney_uwsgi.sh start
-sh ./bin/nohup_run_nodejs_server.sh
-sh ./bin/nohup_run_spider.sh
-sh ./bin/nohup_run_django_server.sh
+PROJECT_PATH=$(cd "$(dirname "$0")"; cd ../;pwd)
+echo $PROJECT_PATH
+sh $PROJECT_PATH/bin/eastmoney_uwsgi.sh start
+sh $PROJECT_PATH/bin/nohup_run_nodejs_server.sh
+sh $PROJECT_PATH/bin/nohup_run_spider.sh
+sh $PROJECT_PATH/bin/nohup_run_django_server.sh
